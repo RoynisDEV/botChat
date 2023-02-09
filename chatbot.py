1fbf470cf526ea21c8f4458116b06f1632f0ca47
RoynisDEV <enriqbravo@hotmail.com>
2023-02-09 17:59:52 -0500

    bot OpenIA

---
+++ chatbot.py
@@ -0,0 +1,35 @@
+import openai 
+import pyttsx3
+
+openai.api_key = "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"
+
+conversation = ""
+
+voice_id = 'spanish'
+voice_id2 = 'spanish-latin-am'
+engine = pyttsx3.init()
+rate = engine.getProperty('rate')
+engine.setProperty('rate', rate-50)
+volume = engine.getProperty('volume')
+engine.setProperty('volume', volume+0.50)
+engine.setProperty('voice', voice_id2)
+
+i = 1
+while (i != 0):
+    question = input("humano: ") 
+    conversation += "\nHumano: " + question + "\nAI:"
+    response = openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt = conversation,
+        temperature = 0.0,
+        max_tokens = 150,
+        top_p = 1,
+        frequency_penalty = 0,
+        presence_penalty = 0.6,
+        stop = ["\n", "Humano:", " AI:"]
+    )
+    anwer = response.choices[0].text.strip()
+    conversation += anwer
+    print("AI: " + anwer + "\n") 
+    engine.say(anwer)
+    engine.runAndWait()
---
+++ chatbotGoogle.py
@@ -0,0 +1,31 @@
+import openai 
+from gtts import gTTS
+from playsound import playsound
+from os import remove  
+
+openai.api_key = "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"
+
+conversation = ""
+
+i = 1
+while (i != 0):
+    question = input("humano: ") 
+    conversation += "\nHumano: " + question + "\nAI:"
+    response = openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt = conversation,
+        temperature = 0.0,
+        max_tokens = 150,
+        top_p = 1,
+        frequency_penalty = 0,
+        presence_penalty = 0.6,
+        stop = ["\n", "Humano:", " AI:"]
+    )
+    anwer = response.choices[0].text.strip()
+    conversation += anwer
+    print("AI: " + anwer + "\n")
+    tts = gTTS(anwer, lang='es')
+    tts.save("audio.mp3")
+    SalidaAudio = "audio.mp3"
+    playsound(SalidaAudio)
+    remove('audio.mp3')
